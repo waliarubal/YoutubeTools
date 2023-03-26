f5bee866c1df3a803e7a26234ad290bf8fad74f7
Rubal Walia <walia.rubal@gmail.com>
2023-03-25 22:28:06 -0700

    Initial code commit.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace YoutubeTools;
+
+partial class FrmMain
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        dgvWindows = new DataGridView();
+        colHandle = new DataGridViewTextBoxColumn();
+        colTitle = new DataGridViewTextBoxColumn();
+        colState = new DataGridViewTextBoxColumn();
+        label1 = new Label();
+        txtVideoKey = new TextBox();
+        label2 = new Label();
+        updnInstances = new NumericUpDown();
+        btnRun = new Button();
+        label3 = new Label();
+        updnReplay = new NumericUpDown();
+        btnStop = new Button();
+        ((System.ComponentModel.ISupportInitialize)dgvWindows).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)updnInstances).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)updnReplay).BeginInit();
+        SuspendLayout();
+        // 
+        // dgvWindows
+        // 
+        dgvWindows.AllowUserToAddRows = false;
+        dgvWindows.AllowUserToDeleteRows = false;
+        dgvWindows.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        dgvWindows.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+        dgvWindows.Columns.AddRange(new DataGridViewColumn[] { colHandle, colTitle, colState });
+        dgvWindows.Location = new Point(12, 70);
+        dgvWindows.Name = "dgvWindows";
+        dgvWindows.ReadOnly = true;
+        dgvWindows.RowTemplate.Height = 25;
+        dgvWindows.Size = new Size(600, 359);
+        dgvWindows.TabIndex = 0;
+        // 
+        // colHandle
+        // 
+        colHandle.HeaderText = "Handle";
+        colHandle.Name = "colHandle";
+        colHandle.ReadOnly = true;
+        // 
+        // colTitle
+        // 
+        colTitle.HeaderText = "Title";
+        colTitle.Name = "colTitle";
+        colTitle.ReadOnly = true;
+        // 
+        // colState
+        // 
+        colState.HeaderText = "State";
+        colState.Name = "colState";
+        colState.ReadOnly = true;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(12, 15);
+        label1.Name = "label1";
+        label1.Size = new Size(62, 15);
+        label1.TabIndex = 1;
+        label1.Text = "Video Key:";
+        // 
+        // txtVideoKey
+        // 
+        txtVideoKey.Location = new Point(80, 12);
+        txtVideoKey.Name = "txtVideoKey";
+        txtVideoKey.Size = new Size(160, 23);
+        txtVideoKey.TabIndex = 2;
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(487, 15);
+        label2.Name = "label2";
+        label2.Size = new Size(59, 15);
+        label2.TabIndex = 3;
+        label2.Text = "Instances:";
+        // 
+        // updnInstances
+        // 
+        updnInstances.Location = new Point(552, 13);
+        updnInstances.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+        updnInstances.Name = "updnInstances";
+        updnInstances.Size = new Size(60, 23);
+        updnInstances.TabIndex = 4;
+        updnInstances.Value = new decimal(new int[] { 10, 0, 0, 0 });
+        // 
+        // btnRun
+        // 
+        btnRun.Location = new Point(456, 41);
+        btnRun.Name = "btnRun";
+        btnRun.Size = new Size(75, 23);
+        btnRun.TabIndex = 5;
+        btnRun.Text = "Start";
+        btnRun.UseVisualStyleBackColor = true;
+        btnRun.Click += btnRun_Click;
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(271, 15);
+        label3.Name = "label3";
+        label3.Size = new Size(135, 15);
+        label3.TabIndex = 6;
+        label3.Text = "Replay Check (minutes):";
+        // 
+        // updnReplay
+        // 
+        updnReplay.Location = new Point(412, 12);
+        updnReplay.Maximum = new decimal(new int[] { 30, 0, 0, 0 });
+        updnReplay.Name = "updnReplay";
+        updnReplay.Size = new Size(60, 23);
+        updnReplay.TabIndex = 7;
+        updnReplay.Value = new decimal(new int[] { 2, 0, 0, 0 });
+        // 
+        // btnStop
+        // 
+        btnStop.Enabled = false;
+        btnStop.Location = new Point(537, 41);
+        btnStop.Name = "btnStop";
+        btnStop.Size = new Size(75, 23);
+        btnStop.TabIndex = 8;
+        btnStop.Text = "Stop";
+        btnStop.UseVisualStyleBackColor = true;
+        btnStop.Click += btnStop_Click;
+        // 
+        // FrmMain
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(624, 441);
+        Controls.Add(btnStop);
+        Controls.Add(updnReplay);
+        Controls.Add(label3);
+        Controls.Add(btnRun);
+        Controls.Add(updnInstances);
+        Controls.Add(label2);
+        Controls.Add(txtVideoKey);
+        Controls.Add(label1);
+        Controls.Add(dgvWindows);
+        Name = "FrmMain";
+        Text = "YouTube Tools";
+        FormClosing += FrmMain_FormClosing;
+        Load += FrmMain_Load;
+        ((System.ComponentModel.ISupportInitialize)dgvWindows).EndInit();
+        ((System.ComponentModel.ISupportInitialize)updnInstances).EndInit();
+        ((System.ComponentModel.ISupportInitialize)updnReplay).EndInit();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private DataGridView dgvWindows;
+    private DataGridViewTextBoxColumn colHandle;
+    private DataGridViewTextBoxColumn colTitle;
+    private DataGridViewTextBoxColumn colState;
+    private Label label1;
+    private TextBox txtVideoKey;
+    private Label label2;
+    private NumericUpDown updnInstances;
+    private Button btnRun;
+    private Label label3;
+    private NumericUpDown updnReplay;
+    private Button btnStop;
+}
---
+++ FrmMain.cs
@@ -0,0 +1,179 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace YoutubeTools;
+
+public partial class FrmMain : Form
+{
+    const string BASE_URL = "https://www.youtube.com/watch";
+
+    readonly List<string> _handles;
+    readonly Random _random;
+    bool _isRunning;
+    IWebDriver _driver;
+
+    public FrmMain()
+    {
+        _random = new Random();
+        _handles = new List<string>();
+
+        InitializeComponent();
+    }
+
+    private void FrmMain_Load(object sender, EventArgs e)
+    {
+#if DEBUG
+        txtVideoKey.Text = "liAUm0d3sbE";
+        updnInstances.Value = 5;
+#endif
+    }
+
+    async Task OpenVideo(string key)
+    {
+        if (_handles.Count > 0)
+            _driver
+                .SwitchTo()
+                .NewWindow(WindowType.Window);
+
+        var handle = _driver.CurrentWindowHandle;
+        _handles.Add(handle);
+
+        _driver
+            .Navigate()
+            .GoToUrl($"{BASE_URL}?v={key}");
+
+        await Task.Delay(_random.Next(5, 10) * 1000);
+
+        var videoControls = new WebDriverWait(_driver, TimeSpan.FromSeconds(10))
+            .Until(driver => driver.FindElement(By.ClassName("ytp-chrome-controls")));
+
+        var action = new Actions(_driver);
+        action
+            .MoveToElement(videoControls)
+            .Perform();
+
+        var state = "Unknown";
+        try
+        {
+            var xPath = "//button[contains(@class, 'ytp-play-button ytp-button') and @data-title-no-tooltip='Play']";
+            var btnPlay = GetElement(xPath);
+            btnPlay?.Click();
+
+            xPath = "//button[contains(@class, 'ytp-mute-button ytp-button')]";
+            var btnMute = GetElement(".//button[@class='ytp-mute-button ytp-button']");
+            btnMute?.Click();
+
+            state = "Playing";
+        }
+        catch
+        {
+
+        }
+
+        dgvWindows.BeginInvoke(() => dgvWindows.Rows.Add(handle, _driver.Title, state));
+    }
+
+    IWebElement GetElement(string xPath, int waitSeconds = 20)
+    {
+        var waitTime = TimeSpan.FromSeconds(waitSeconds);
+        try
+        {
+            return new WebDriverWait(_driver, waitTime)
+                .Until(driver => driver.FindElement(By.XPath(xPath)));
+        }
+        catch (Exception ex) when (
+            ex is NoSuchElementException ||
+            ex is WebDriverTimeoutException)
+        {
+            return null;
+        }
+    }
+
+    void Stop()
+    {
+        if (!_isRunning)
+            return;
+
+        _isRunning = false;
+        btnStop.Enabled = _isRunning;
+        btnRun.Enabled = !_isRunning;
+
+        _driver.Quit();
+        _driver.Dispose();
+
+        dgvWindows.Rows.Clear();
+    }
+
+    async Task Start()
+    {
+        var config = new ChromeConfig();
+
+        var manager = new DriverManager();
+        manager.SetUpDriver(config);
+
+        var options = new ChromeOptions();
+        options.AddArgument("--mute-audio");
+        options.AddArgument("--incognito");
+        options.AddArgument("disable-infobars");
+        options.AddExcludedArgument("enable-automation");
+        options.AddAdditionalChromeOption("useAutomationExtension", false);
+
+        var service = ChromeDriverService.CreateDefaultService();
+        service.HideCommandPromptWindow = true;
+
+        _driver = new ChromeDriver(service, options);
+        _driver
+            .Manage()
+            .Window
+            .Size = new Size(1024, 768);
+
+        var videoKey = txtVideoKey.Text;
+        var instances = updnInstances.Value;
+        for (var count = 1; count < instances; count++)
+            await OpenVideo(videoKey);
+
+        _isRunning = true;
+        btnStop.Enabled = _isRunning;
+        btnRun.Enabled = !_isRunning;
+
+        var replayCheckInSeconds = (int)updnReplay.Value * 1000 * 60;
+        if (replayCheckInSeconds == 0)
+            return;
+
+        await Task.Run(() =>
+        {
+            if (!_isRunning)
+                return;
+
+            dgvWindows.BeginInvoke(() =>
+            {
+                foreach (DataGridViewRow row in dgvWindows.Rows)
+                {
+                    var handle = row.Cells[0].Value.ToString();
+
+                    _driver
+                        .SwitchTo()
+                        .Window(handle);
+
+                    var xPath = "//button[contains(@class, 'ytp-play-button ytp-button') and @data-title-no-tooltip='Play']";
+                    var btnPlay = GetElement(xPath);
+                    btnPlay?.Click();
+
+                    row.Cells[2].Value = "Playing";
+                }
+            });
+
+            Task.Delay(replayCheckInSeconds);
+        });
+    }
+
+    private void FrmMain_FormClosing(object sender, FormClosingEventArgs e) => Stop();
+
+    private async void btnRun_Click(object sender, EventArgs e) => await Start();
+
+    private void btnStop_Click(object sender, EventArgs e) => Stop();
+}
